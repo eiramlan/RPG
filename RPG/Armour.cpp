3ad709df3522dd7668dfe29af28b0ec8b91b71d7
eiramlan <54712090+eiramlan@users.noreply.github.com>
2020-11-17 13:48:02 +0000

    Update Armour.cpp

--- RPG/Armour.cpp
+++ RPG/Armour.cpp
@@ -3,53 +3,35 @@
 Armour::Armour() {
 }
 
-Armour::Armour(std::string armourName, int value, float weight, int defence, int health, ArmourType armourType) :
-	Item{ armourName, value, weight }, defence_{ defence }, armourHealth_{ health }, armourType_{ armourType } {
+Armour::Armour(std::string armourName, int value, float weight, int defence, int health, ArmourType armourType) {
 
 }
 
 void Armour::SetArmourDefence(int defence) {
-	defence_ = defence;
+	
 }
 
 int Armour::GetArmourDefence() const {
-	return defence_;
+	
 }
 
 void Armour::SetArmourHealth(int health) {
-	armourHealth_ = health;
+	
 }
 
 int Armour::GetArmourHealth() const {
-	return armourHealth_;
+	
 }
 
 void Armour::SetArmourType(ArmourType armourType) {
-	armourType_ = armourType;
+	
 }
 
 ArmourType Armour::GetArmourType() const {
-	return armourType_;
+	
 }
 
 std::string Armour::ToString() {
-	std::string tmpStr{ "" };
-	std::string aType{ "" };
 
-	switch (armourType_) {
-	case ArmourType::Cardboard: aType = "Cardboard"; 
-		break;
-	case ArmourType::Leather: aType = "Leather";
-		break;
-	case ArmourType::Wood: aType = "Wood";
-		break;
-	case ArmourType::Iron: aType = "Iron";
-		break;
-	case ArmourType::Steel: aType = "Steel";
-		break;
-	}
-
-	tmpStr.append("Armour: " + Item::ToString() + ", Defence: " + std::to_string(defence_) + ", Armour health: " + std::to_string(armourHealth_) + ", Armour Material: " + aType + "\n");
-	return tmpStr;
 }
 
