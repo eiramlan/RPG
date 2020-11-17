c3ce2c5c1db006f2a83b1bea5306bf7d9bd14509
eiramlan <54712090+eiramlan@users.noreply.github.com>
2020-11-17 13:47:29 +0000

    Update Item.cpp

--- RPG/Item.cpp
+++ RPG/Item.cpp
@@ -5,36 +5,33 @@ Item::Item() {
 
 }
 
-Item::Item(std::string itemName, int value, float weight) :
-	itemName_{ itemName }, itemValue_{ value }, weight_{ weight } {
+Item::Item(std::string itemName, int value, float weight) {
 }
 
 void Item::SetItemName(std::string itemName) {
-	itemName_ = itemName;
+	
 }
 
 std::string Item::GetItemName() const {
-	return itemName_;
+	
 }
 
 void Item::SetItemValue(int value) {
-	itemValue_ = value;
+	
 }
 
 int Item::GetItemValue() const {
-	return itemValue_;
+	
 }
 
 void Item::SetItemWeight(float weight) {
-	weight_ = weight;
+	
 }
 
 float Item::GetItemWeight() const {
-	return weight_;
+	
 }
 
 std::string Item::ToString() {
-	std::string tmpStr{ "" };
-	tmpStr.append(itemName_ + ", Worth " + std::to_string(itemValue_) + " Gold coins");
-	return tmpStr;
+	
 }
